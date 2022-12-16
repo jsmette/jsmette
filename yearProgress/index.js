c860cb3a1ce5cf132eda84f3c0d62f2aed9a6dbb
Julien Smette <16221850+jsmette@users.noreply.github.com>
2022-12-16 10:45:38 +0100

    Update index.js

--- yearProgress/index.js
+++ yearProgress/index.js
@@ -13,8 +13,6 @@ function generateProgressBar() {
     return `{ ${progressBar} }`
 }
 
-const readme = `
-Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-Updated on ${new Date().toUTCString()}`
+const readme = '⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % ⏰ Updated on ${new Date().toUTCString()}'
 
 console.log(readme)
